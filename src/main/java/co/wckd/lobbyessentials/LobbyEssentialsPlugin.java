d79bbb61b8b813c128a7e7feec7519fe5bdbc725
devwckd <dev.wckd@gmail.com>
2020-05-26 20:07:36 -0300

    First Commit 🍕

---
+++ src/main/java/co/wckd/lobbyessentials/LobbyEssentialsPlugin.java
@@ -0,0 +1,12 @@
+package co.wckd.lobbyessentials;
+
+import co.wckd.boilerplate.plugin.BoilerplatePlugin;
+
+public class LobbyEssentialsPlugin extends BoilerplatePlugin {
+
+    @Override
+    public void enable() {
+
+    }
+
+}
